b48485d8e568ba7652e7096759f6c355e354cf96
Kaio <rasec.k0@proton.me>
2023-10-14 21:20:49 -0300

    Initial release

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,23 +5,22 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: '',
+			logo: {
+				src: './src/assets/docs.png'
+			},
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+			customCss: [
+				'./src/styles/custom.css',
+				'@fontsource/atkinson-hyperlegible/400.css',
+				'@fontsource/atkinson-hyperlegible/700.css',
+				'@fontsource/atkinson-hyperlegible/400-italic.css',
+				'@fontsource/atkinson-hyperlegible/700-italic.css',
+				'@fontsource-variable/jetbrains-mono',
+				'@fontsource-variable/jetbrains-mono/wght-italic.css',
+			]
 		}),
 	],
 });
