21edfbd6ed3839efd7436552c1307486689fda78
Kaio <rasec.k0@proton.me>
2023-10-26 20:13:56 -0300

    Rewrite server status widget to actually work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,21 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: '',
-			logo: {
-				src: './src/assets/docs.png'
-			},
-			social: {
-				github: 'https://github.com/AVTM0/avtmsmp-docs',
-			},
-			customCss: [
-				'./src/styles/custom.css',
-				'@fontsource/atkinson-hyperlegible/400.css',
-				'@fontsource/atkinson-hyperlegible/700.css',
-				'@fontsource/atkinson-hyperlegible/400-italic.css',
-				'@fontsource/atkinson-hyperlegible/700-italic.css',
-				'@fontsource-variable/jetbrains-mono',
-				'@fontsource-variable/jetbrains-mono/wght-italic.css',
-			],
-			components: {
-				SocialIcons: './src/components/CustomSocial.astro'
-			}
-		}),
-	],
+  integrations: [starlight({
+    title: '',
+    logo: {
+      src: './src/assets/docs.png'
+    },
+    social: {
+      github: 'https://github.com/AVTM0/avtmsmp-docs'
+    },
+    customCss: ['./src/styles/custom.css', '@fontsource/atkinson-hyperlegible/400.css', '@fontsource/atkinson-hyperlegible/700.css', '@fontsource/atkinson-hyperlegible/400-italic.css', '@fontsource/atkinson-hyperlegible/700-italic.css', '@fontsource-variable/jetbrains-mono', '@fontsource-variable/jetbrains-mono/wght-italic.css'],
+    components: {
+      SocialIcons: './src/components/CustomSocial.astro'
+    }
+  }), vue()]
 });
